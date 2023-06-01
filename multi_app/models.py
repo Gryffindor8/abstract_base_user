917b265f0313b369286416b7ea1aab380cd0bb82
Hasnain Khan <muhammad.hasnain@mlsense.ai>
2023-06-01 13:46:31 +0500

    gitignore updated

--- multi_app/models.py
+++ multi_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ["first_name"]
+    objects = MyUserManager()
+
+    def has_perm(self, perm, obj=None):  # noqa
+        # For now, allow all permissions
+        return True
+
+    def has_module_perms(self, app_label):  # noqa
+        # For now, allow all app modules
+        return True
